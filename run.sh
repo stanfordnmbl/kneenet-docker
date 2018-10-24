b72a892e941917e5c1d86ed1ede91cf8f32ace3c
Lukasz Kidzinski <lukasz.kidzinski@gmail.com>
2018-10-24 13:42:06 -0700

    Download docker

--- run.sh
+++ run.sh
@@ -1,11 +1,12 @@
 if [ ! -f models/KneeNet.0 ]; then
     mkdir -p models
+    mkdir -p output
     wget -O models/KneeNet.0 https://s3-eu-west-1.amazonaws.com/kidzinski/models/KneeNet/KneeNet.0
 fi
-sudo nvidia-docker run \
+docker run \
     -v $PWD/input:/workspace/input \
     -v $PWD/output:/workspace/output \
     -v $PWD/scripts:/workspace/scripts \
     -v $PWD/models:/workspace/models \
-    -it stanford-nmbl/kneenet:latest \
+    -it kidzik/kneenet:latest \
     python scripts/predict.py && printf "\n-- RESULTS (in output/prediction.csv) --\n" && cat output/predictions.csv | column -t -s,
