c9b5fa6cf828eb835e59c61d978da0d0ae2bfd29
Łukasz Kidziński <lukasz.kidzinski@gmail.com>
2018-11-07 14:01:51 -0800

    Adapted to MacOS and Linux

--- run.sh
+++ run.sh
@@ -1,12 +1,12 @@
 if [ ! -f models/KneeNet.0 ]; then
     mkdir -p models
     mkdir -p output
-    wget -O models/KneeNet.0 https://s3-eu-west-1.amazonaws.com/kidzinski/models/KneeNet/KneeNet.0
+    curl https://s3-eu-west-1.amazonaws.com/kidzinski/models/KneeNet/KneeNet.0 -O models/KneeNet.0 
 fi
 docker run \
-    -v $PWD/input:/workspace/input \
-    -v $PWD/output:/workspace/output \
-    -v $PWD/scripts:/workspace/scripts \
-    -v $PWD/models:/workspace/models \
+    -v ${PWD}/input:/workspace/input \
+    -v ${PWD}/output:/workspace/output \
+    -v ${PWD}/scripts:/workspace/scripts \
+    -v ${PWD}/models:/workspace/models \
     -it kidzik/kneenet:latest \
     python scripts/predict.py && printf "\n-- RESULTS (in output/prediction.csv) --\n" && cat output/predictions.csv | column -t -s,
